f53b08a62f0c9513c8255ed655df9f90af912c6d
Oleh <olegbrogina@gmail.com>
2024-09-26 21:59:34 +0300

    update

--- src/main/java/com/example/cqrsbankingapp/domain/model/ObjectConverter.java
+++ src/main/java/com/example/cqrsbankingapp/domain/model/ObjectConverter.java
@@ -1,29 +1,31 @@
 package com.example.cqrsbankingapp.domain.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import lombok.SneakyThrows;
 
-/**
- * @SneakyThrows allows bypassing the need to explicitly handle checked exceptions,
- * such as JsonProcessingException, which might be thrown by ObjectMapper methods.
- * It reduces boilerplate code but should be used with caution, as it hides potential exceptions.
- */
 
 @Converter
 public class ObjectConverter implements AttributeConverter<Object, String> {
 
-    @SneakyThrows
     @Override
     public String convertToDatabaseColumn(Object attribute) {
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(attribute);
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting object to JSON", e);
+        }
     }
 
     @Override
     public Object convertToEntityAttribute(String dbData) {
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.convertValue(dbData, Object.class);
+        try {
+            return objectMapper.readValue(dbData, Object.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting JSON to object", e);
+        }
     }
 }
